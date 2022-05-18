c1739f41afea5d09d2e5675a9975890f95bca25b
oleg <opa@exante.eu>
2022-05-19 01:10:53 +0300

    Remove script api.

--- libs/shared/src/storage/storage.module.ts
+++ libs/shared/src/storage/storage.module.ts
@@ -3,12 +3,11 @@ import { StorageService } from './storage.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { SiteModel } from '@app/shared/storage/models/site.model';
 import { FileModel } from '@app/shared/storage/models/file.model';
-import { ScriptModel } from '@app/shared/storage/models/script.model';
 import { AccountModel } from '@app/shared/storage/models/account.model';
 import { VoteModel } from '@app/shared/storage/models/vote.model';
 
 @Module({
-    imports: [SequelizeModule.forFeature([SiteModel, FileModel, ScriptModel, AccountModel, VoteModel])],
+    imports: [SequelizeModule.forFeature([SiteModel, FileModel, AccountModel, VoteModel])],
     providers: [StorageService],
     exports: [StorageService],
 })
--- libs/shared/src/storage/storage.service.ts
+++ libs/shared/src/storage/storage.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { SiteModel } from '@app/shared/storage/models/site.model';
 import { FileModel } from '@app/shared/storage/models/file.model';
-import { ScriptModel } from '@app/shared/storage/models/script.model';
 import { AccountModel } from '@app/shared/storage/models/account.model';
 
 @Injectable()
@@ -10,7 +9,6 @@ export class StorageService {
     constructor(
         @InjectModel(SiteModel) public Site: typeof SiteModel,
         @InjectModel(FileModel) public File: typeof FileModel,
-        @InjectModel(ScriptModel) public Script: typeof ScriptModel,
         @InjectModel(AccountModel) public Account: typeof AccountModel,
     ) {}
 }
